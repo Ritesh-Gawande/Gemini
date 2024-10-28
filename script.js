fc23d8dab6c0ad018b52d05959b79e56058169d3
Ritesh Gawande <106462362+Ritesh-Gawande@users.noreply.github.com>
2024-10-28 12:16:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const clearChatButton = document.getElementById("deleteButton");
 let currentUserMessage = null;
 let isGeneratingResponse = false;
 
-const GOOGLE_API_KEY = "AIzaSyBAbahQYCUg8PQso_Hf4M9PGfImvJg3mp8";
+const GOOGLE_API_KEY = "-";
 const API_REQUEST_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${GOOGLE_API_KEY}`;
 
 // Load saved data from local storage
